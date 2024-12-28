0a7a6ee77877006df1e3ffc7d735cb6025d427d6
Lech <ls.inne@pm.me>
2024-12-28 20:17:07 +0100

    Zmiany w gałęzi poprawki

--- wyszukiwanie_z_konwersją2.py
+++ wyszukiwanie_z_konwersją2.py
@@ -4,7 +4,8 @@ import re
 import pandas as pd
 
 #zmiany
-# zmiany w master 29
+#kolejne zmiany w gałęzi poprawki
+
 def pdf_to_images_and_html_with_responsive_anchors(pdf_path, pattern, output_html_dir):
     pdf_document = fitz.open(pdf_path)
     output_html = os.path.join(output_html_dir, f"{os.path.splitext(os.path.basename(pdf_path))[0]}.html")
